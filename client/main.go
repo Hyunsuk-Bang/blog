89a2e7657abf8b917ecd06c934f8a17fca36d9f6
Hyunsuk <hbnag3@hawk.iit.edu>
2022-09-09 22:38:30 -0500

    dial to grpc server

---
+++ client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	pb "blog/proto"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("CLIENT: Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewBlogServiceClient(conn)
+
+	createBlog(c)
+}
